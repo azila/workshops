e1270a610e5f771e6ce810afc5631e547208941f
Liza <korotchenko.liza@gmail.com>
2014-11-27 16:45:06 +0100

    [#4] finished issue 4

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,15 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, numericality: true, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }
+
+  def average_rating
+    average = 0.0
+    reviews.each { |review| average += review.rating }
+    average / reviews.count
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(email: 'one@something.com', password: '12345678', firstname: 'One', lastname: 'First')
+User.create!(email: 'two@something.com', password: '12345678', firstname: 'Two', lastname: 'Second')
+User.create!(email: 'three@something.com', password: '12345678', firstname: 'Three', lastname: 'Third')
+User.create!(email: 'four@something.com', password: '12345678', firstname: 'Four', lastname: 'Fourth')
+User.create!(email: 'five@something.com', password: '12345678', firstname: 'Five', lastname: 'Fifth')
+User.create!(email: 'six@something.com', password: '87654321', firstname: 'Six', lastname: 'Sixth', admin: true)
+
+Category.create!(name: 'Series')
+
+Product.create!(title: 'The Big Bang Theory', description: 'Mensa-fied best friends and roommates Leonard and Sheldon, 
+          physicists who work at the California Institute of Technology...', price: '100.00', category_id: 1, user_id: 1)
+Product.create!(title: 'Breaking Bad', description: 'Mild-mannered high school chemistry teacher Walter White thinks his
+          life cannot get much worse...', price: '100.00', category_id: 1, user_id: 1)
+Product.create!(title: 'House of Cards', description: 'U.S. Rep. Francis Underwood of South Carolina starts out as a
+          ruthless politician...', price: '100.00', category_id: 1, user_id: 1)
